824c908efaf47467fabf62d0c50c197fbf33959a
Waldo Hidalgo <whidalgohp@gmail.com>
2024-03-21 17:54:12 -0300

    cambio puerto a 4000

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 const exphbs = require("express-handlebars");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 const rutas = require("./routes");
 
