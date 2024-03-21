41104b1b9a121e3ef0eaf96cf67fc3b92989d669
Waldo Hidalgo <whidalgohp@gmail.com>
2024-03-21 18:48:25 -0300

    agrego permisos

--- routes.js
+++ routes.js
@@ -10,7 +10,18 @@ router.use("/jquery", express.static("./node_modules/jquery/dist"));
 router.use("/public", express.static("./public"));
 
 router.use((req, res, next) => {
-  const directorio = "/archivos";
+  const directorio = "archivos";
+  fs.chmod(directorio, 0o777, (err) => {
+    if (err) {
+      console.error("Error al establecer los permisos:", err);
+      res.status(500).send("Error al establecer permisos");
+    }
+
+    console.log(
+      "Permisos establecidos correctamente en el directorio:",
+      directorio
+    );
+  });
   fs.readdir(directorio, (err, archivos) => {
     if (err) {
       console.error("Error al leer el directorio:", err);
