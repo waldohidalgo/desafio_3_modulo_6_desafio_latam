d320a9016897f8ad5fde2f4fb31694f6d2752116
Waldo Hidalgo <whidalgohp@gmail.com>
2024-03-21 17:59:21 -0300

    edito directorio

--- routes.js
+++ routes.js
@@ -10,7 +10,7 @@ router.use("/jquery", express.static("./node_modules/jquery/dist"));
 router.use("/public", express.static("./public"));
 
 router.use((req, res, next) => {
-  const directorio = "archivos";
+  const directorio = "/archivos";
   fs.readdir(directorio, (err, archivos) => {
     if (err) {
       console.error("Error al leer el directorio:", err);
