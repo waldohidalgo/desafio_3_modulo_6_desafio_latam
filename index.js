553c254060457aba6cc72465a75d3a0d8ffbf2bf
Waldo Hidalgo <whidalgohp@gmail.com>
2024-03-21 17:46:38 -0300

    configure vercel.json

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 const exphbs = require("express-handlebars");
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 const rutas = require("./routes");
 
@@ -31,3 +31,4 @@ app.listen(PORT, () => {
 });
 
 app.use("/", rutas);
+module.exports = app;
